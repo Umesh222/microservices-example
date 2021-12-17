4a462c0871ac6c2c04f41035edd4173ab76394c1
Umesh222 <46746007+Umesh222@users.noreply.github.com>
2021-12-17 19:04:57 +0530

    tested

---
+++ src/main/java/com/umesh/microservices/example/MicroservicesExampleApplication.java
@@ -0,0 +1,14 @@
+package com.umesh.microservices.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesExampleApplication.class, args);
+		//System.out.println("tested...");
+	}
+
+}
